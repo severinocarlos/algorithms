ece5e975da937c760e94672ea7ea21e35d62038e
severinocarlos <juninho_456@hotmail.com>
2023-04-13 00:43:34 -0300

    (add): segment tree

---
+++ data-structures/segment_tree.cpp
@@ -0,0 +1,44 @@
+const int ms = 2 * 1e5 + 5;
+vector<ull> seg(4*ms);
+int n;
+
+void build(vector<int>& v, int l, int r, int idx) {
+	if (l == r) {
+		seg[idx] = v[l];
+		return;
+	}
+
+	int m = (l+r) / 2;
+	int left = 2*idx + 1;
+	int right = 2*idx + 2;
+
+	build(v, l, m, left), build(v, m+1, r, right);
+
+	seg[idx] = seg[left] + seg[right];
+}
+
+ll query(int L, int R, int l, int r, int idx) {
+	if (R < l || L > r) return 0;
+	if (L <= l && r <= R) return seg[idx];
+
+	int m = (l+r) / 2;
+	int left = 2*idx + 1;
+	int right = 2*idx + 2;
+
+	return query(L,R, l, m, left) + query(L,R, m+1, r, right);
+}
+
+void update(int pos, ll num, int l, int r, int idx) {
+	if (l == r) {
+		seg[idx] = num;
+		return;
+	}
+	int m = (l+r) / 2;
+	int left = 2*idx + 1;
+	int right = 2*idx + 2;
+
+	if (pos <= m) update(pos, num, l, m, left);
+	else update(pos, num, m+1, r, right);
+
+	seg[idx] = seg[left] + seg[right];
+}
