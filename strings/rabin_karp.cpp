f763059b056d2260a8689ce12bbfaa1b587baba2
severinocarlos <juninho_456@hotmail.com>
2023-04-13 00:47:32 -0300

    (add): rabin karp

---
+++ strings/rabin_karp.cpp
@@ -0,0 +1,30 @@
+ll base, mod;
+vector<ll> pot, has;
+
+
+Rabin(string &str, ll base = 997, ll mod = 1e9+7): mod(mod), base(base) {
+	ll sz = str.size();
+	pot.resize(sz+1);
+	has.resize(sz+1);
+
+
+	build(str);
+}
+
+void build(string &str) {
+	pot[0] = 1;
+	has[0] = str[0];
+
+	for (int i = 1; i < str.size(); i++) {
+		pot[i] = (pot[i-1]*base) % mod;
+		has[i] = ((has[i-1]*base) + str[i]) % mod;
+	}
+}
+
+
+ll getkey(ll l, ll r) {
+	ll ans = has[r];
+
+	if (l > 0) ans = ((ans - pot[r - l + 1] * has[l-1]) % mod + mod) % mod;
+	return ans;
+}
